7ecbec101fb9648bdd4cc09476bdb6aeba198393
PenguinCabinet <kage2.kage22@gmail.com>
2022-08-18 22:19:48 +0900

    Init

---
+++ measure.d
@@ -0,0 +1,27 @@
+module measure;
+import std.stdio,std.process,std.datetime.date,std.datetime.stopwatch;
+
+void measure_time(string v1,string v2){
+    executeShell(v1);
+    writeln(v1);
+    float time;
+    auto sw = StopWatch(AutoStart.yes);
+    sw.start();
+    foreach (i; 0..10)
+    {
+        executeShell(v2);
+    }
+    auto t1 = sw.peek();
+    auto temp=t1;
+    auto temp2=temp.total!"seconds"/10;
+    writefln("time:%d Second",temp2);
+    //writeln(executeShell(v).output);
+}
+
+void main(){
+    measure_time("dmd Brainfuxk-D/source/app","app Brainfuxk-D/test/Correct-Cases/mandelbrot.bf");
+    measure_time("dmd -O -release -inline Brainfuxk-D/source/app","app Brainfuxk-D/test/Correct-Cases/mandelbrot.bf");
+    measure_time("ldc2 Brainfuxk-D/source/app","app Brainfuxk-D/test/Correct-Cases/mandelbrot.bf");
+    measure_time("ldc2 -O3 Brainfuxk-D/source/app","app Brainfuxk-D/test/Correct-Cases/mandelbrot.bf");
+
+}
